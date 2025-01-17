d77b8003af08a9e9727650513f42ee939ac31553
Douglas Silva <douglasitpro@gmail.com>
2025-01-16 22:48:01 -0300

    Configurando banco de dados

---
+++ utils/db.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Banco de dados conectado com sucesso');
+    } catch (error) {
+        console.error('Erro ao conectar no banco de dados:', error.message);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
