ed8847c47c86c6dbd1becc837f45e2e8616cd767
Douglas Silva <douglasitpro@gmail.com>
2025-01-17 20:34:59 -0300

    Ajustando as expotações de cada função

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,8 +8,8 @@ const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 };
 
-//Registro de usuário
-const registerUser = async (req, res) => {
+// Registro de usuário
+export const registerUser = async (req, res) => {
     const { name, email, password } = req.body;
 
     try {
@@ -18,13 +18,14 @@ const registerUser = async (req, res) => {
 
         const user = await User.create({ name, email, password });
         res.status(201).json({
-            id: user_id,
+            id: user._id,
             name: user.name,
             email: user.email,
-            token: generateToken(user_id),
+            token: generateToken(user._id),
         });
     } catch (error) {
-        res.status(500).json({ message: "Erro no servidor", error});
+        res.status(500).json({ message: 'Erro no servidor', error });
     }
 };
+
 
