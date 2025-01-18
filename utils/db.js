996e265e7227ce1c99434729b133f40958246956
Douglas Silva <douglasitpro@gmail.com>
2025-01-18 19:12:22 -0300

    Configurando banco de dados de desenvolvimento

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI_DEV);
         console.log('Banco de dados conectado com sucesso');
     } catch (error) {
         console.error('Erro ao conectar no banco de dados:', error.message);
